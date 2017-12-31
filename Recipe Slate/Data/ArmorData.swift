6d670d915983050496300e783cd3d296dd142c0c
mrtnlst <martinlist@me.com>
2017-12-31 18:04:45 +0100

    - added new ArmorSet type and new TableViewController for Upgrades

---
+++ Recipe Slate/Data/ArmorData.swift
@@ -0,0 +1,13 @@
+//
+//  ArmorData.swift
+//  Recipe Slate
+//
+//  Created by martin on 31.12.17.
+//  Copyright © 2017 Martin List. All rights reserved.
+//
+
+import Foundation
+
+let armorSetData = [ArmorSet(setName: "Flamebraker Set",
+                             setPieces: ["Flamebraker Helm", "Flamebraker Armor", "Flamebraker Boots"],
+                             defaultEffect: "Flame Guard", setEffect: "Fire Proof")]
